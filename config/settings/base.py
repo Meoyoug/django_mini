52ae5e29cccbfeb2b62061b4fbf07654f010865d
meoyoug <eodyd426@gmail.com>
2024-09-12 15:10:26 +0900

    Config: 데이터베이스 연결, 배포-개발 환경 분리, 환경변수 읽어오기
    - config / settings /
      - 데이터 베이스 연결
      - 배포 - 개발 환경 설정 파일 분리
      - settings.py 심볼릭 링크 생성
    
    - manage.py
      - DJANGO_SETTINGS_MODULE 값을 분리된 settings에 맞게 수정

---
+++ config/settings/base.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+import json
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+with open(BASE_DIR / ".secret.json") as f:
+    config_secret_str = f.read()
+SECRET = json.loads(config_secret_str)
+
+SECRET_KEY = SECRET["DJANGO"]["SECRET_KEY"]
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": SECRET["POSTGRES"]["HOST"],
+        "USER": SECRET["POSTGRES"]["USER"],
+        "PASSWORD": SECRET["POSTGRES"]["PASSWORD"],
+        "NAME": SECRET["POSTGRES"]["NAME"],
+        "PORT": 5432,
+    }
+}
+
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
